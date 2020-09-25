e9e58acc133ab69e1e147c311e905cea7c94848d
Rick Gleason <fcgleason@tgp-architects.com>
2020-09-25 09:07:45 -0400

    v0.4.0.13 chg plugin GL JSON NMEA

--- cmake/PluginGL.cmake
+++ cmake/PluginGL.cmake
@@ -17,15 +17,15 @@ SET(SRC_PLUGINGL
 #          src/GL/glext.h
 # )
 
-if(UNIX)
-    add_definitions("-fpic")
-endif(UNIX)
+#if(UNIX)
+#    add_definitions("-fpic")
+#endif(UNIX)
 
-message(STATUS "PROJECT_SOURCE_DIR: ${PROJECT_SOURCE_DIR}")
-include_directories(BEFORE ${PROJECT_SOURCE_DIR}/src/plugingl)
-include_directories(BEFORE ${PROJECT_SOURCE_DIR}/libs/GL) #added for watchdog
+#message(STATUS "PROJECT_SOURCE_DIR: ${PROJECT_SOURCE_DIR}")
+#include_directories(BEFORE ${PROJECT_SOURCE_DIR}/src/plugingl)
+#include_directories(BEFORE ${PROJECT_SOURCE_DIR}/libs/GL) #added for watchdog
 
-ADD_LIBRARY(${PACKAGE_NAME}_LIB_PLUGINGL STATIC ${SRC_PLUGINGL})
+#ADD_LIBRARY(${PACKAGE_NAME}_LIB_PLUGINGL STATIC ${SRC_PLUGINGL})
 
-TARGET_LINK_LIBRARIES( ${PACKAGE_NAME} ${PACKAGE_NAME}_LIB_PLUGINGL )
-message(STATUS "Add Library ${PACKAGE_NAME}_LIB_PLUGINGL")  #added for Watchdog
+#TARGET_LINK_LIBRARIES( ${PACKAGE_NAME} ${PACKAGE_NAME}_LIB_PLUGINGL )
+#message(STATUS "Add Library ${PACKAGE_NAME}_LIB_PLUGINGL")  #added for Watchdog
