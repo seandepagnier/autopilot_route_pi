4e5a14ec2edd4f03113295d21c2d119357137316
Rick Gleason <fcgleason@tgp-architects.com>
2023-05-03 19:36:03 -0400

    0.4.12.0 + TP1.0.237

--- bld.bat
+++ bld.bat
@@ -20,7 +20,26 @@ REM cmake -T v143 -A Win32 -DOCPN_TARGET=MSVC "Visual Studio 17 2022" ^
 REM      -DCMAKE_BUILD_TYPE=RelWithDebInfo  ..
 REM cmake --build . --target tarball --config RelWithDebInfo >output.txt
 
-REM  For Opencpn 5.7.1 and wxWidgets-3.2.1
+REM  Setup for wxWidgets 3.2.2
+REM  wxWidgets_INCLUDE_DIRS   %WXWIN%\include
+REM  wxWidgets_LIB_DIR   %WXWIN%\lib\vc_dll
+REM  wxWidgets_LIBRARIES    %WXWIN%\lib\vc_dll
+REM  wxWidgets_ROOT_DIR   %WXWIN%
+REM  WXWIN   C:\Users\fcgle\source\repos2\OpenCPN\cache\buildwxWidgets\
+
+REM configdev58.bat
+REM Can use  "call configdev58.bat"
+REM set "wxDIR=C:\Users\fcgle\source\repos2\OpenCPN\buildwin\..\cache\buildwxWidgets" 
+REM set "wxWidgets_ROOT_DIR=C:\Users\fcgle\source\repos2\OpenCPN\buildwin\..\cache\buildwxWidgets" 
+REM set "wxWidgets_LIB_DIR=C:\Users\fcgle\source\repos2\OpenCPN\buildwin\..\cache\buildwxWidgets\lib\vc_dll" 
+
+REM  Copied built wxWidgets-3.2.2 to C:\Users\fcgle\source\wxWidgets-3.2.2
+set "wxDIR=%WXWIN%
+set "wxWidgets_ROOT_DIR=%WXWIN% 
+set "wxWidgets_LIB_DIR=%WXWIN%\lib\vc14x_dll" 
+set "WXWIN=C:\Users\fcgle\source\wxWidgets-3.2.2"
+
+REM  For Opencpn 5.8 and wxWidgets-3.2.2
 
 
 cd build
